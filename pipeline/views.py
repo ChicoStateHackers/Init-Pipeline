b775e68e70690fbbdbba3813953a3c786a912e17
Micah Perez <micahperez99@gmail.com>
2022-03-26 16:20:34 -0700

    style: add change to instruction colors

--- pipeline/views.py
+++ pipeline/views.py
@@ -7,37 +7,23 @@ from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home(request):
-     #subprocess.call(["g++", "ex.cpp"], cwd=STATIC_DIR)
-     #tmp=subprocess.call("./a.out", cwd=STATIC_DIR) 
-     #my_dict = {}
-    # if request.method == 'POST':
-    #     form = UploadFileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         handle_uploaded_file(request.FILES['file'])
-    #         #return HttpResponseRedirect('/success/url/')
-    #         # Cycle_Data.objects.all().delete()
-    #         # file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
-    #         # instruction_data = list(csv.reader(open(file_path)))
-    #         # for row in instruction_data:
-    #         #     Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
-    #         # my_list = Cycle_Data.objects.all()
-    #         print("Made it")
-    #         #return render(request, 'home.html', {'instruction_data': my_list})
-    #         Cycle_Data.objects.all().delete()
-    #         return render(request, 'home.html', {'instruction_data': my_list})
-    #     else:
-    #         print("fuck")
-
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+        #subprocess.call(["g++", "ex.cpp"], cwd=PARSER_DIR)
+        subprocess.call("make", cwd=PARSER_DIR)
+        tmp=subprocess.call("./run", cwd=PARSER_DIR)
         #file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
         print("lets go!")
-        # return render(request, 'core/simple_upload.html', {
-        #     'uploaded_file_url': uploaded_file_url
-        # })
+        Cycle_Data.objects.all().delete()
+        file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
+        instruction_data = list(csv.reader(open(file_path)))
+        for row in instruction_data:
+            Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
+        my_list = Cycle_Data.objects.all()
+        return render(request, 'home.html', {'instruction_data': my_list})
 
 
     Cycle_Data.objects.all().delete()
@@ -47,3 +33,4 @@ def home(request):
         Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
     my_list = Cycle_Data.objects.all()
     return render(request, 'home.html', {'instruction_data': my_list})
+
