c6f1e2ba5e7e0c705c15a726933a423923801e0f
ColinAF <cafrisch98@gmail.com>
2022-03-26 02:58:19 -0700

    More stubs for program object

---
+++ parser/program.cpp
@@ -0,0 +1,27 @@
+#include "program.h"
+
+Program::Program(std::string file_name)
+{
+
+    return;
+};
+
+Program::~Program()
+{
+
+};
+
+State Program::go_to_line()
+{
+
+};
+
+State Program::step_cycle()
+{
+
+};
+State Program::back_cycle()
+{
+
+
+};
