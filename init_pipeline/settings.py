5ce1a1e1524ea37f80096ac666fdf491c3b6c619
Micah Perez <micahperez99@gmail.com>
2022-03-26 15:41:55 -0700

    fix: change dynamic html for instrucitons

--- init_pipeline/settings.py
+++ init_pipeline/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ STATIC_DIR ]
 
+MEDIA_URL = '/parser/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "parser")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- pipeline/views.py
+++ pipeline/views.py
@@ -2,6 +2,8 @@ from init_pipeline.settings import STATIC_DIR, PARSER_DIR
 from django.shortcuts import render
 import subprocess, csv, os
 from pipeline.models import Cycle_Data
+from pipeline.forms import UploadFileForm
+from django.core.files.storage import FileSystemStorage
 
 
 # Create your views here.
@@ -9,14 +11,40 @@ def home(request):
      #subprocess.call(["g++", "ex.cpp"], cwd=STATIC_DIR)
      #tmp=subprocess.call("./a.out", cwd=STATIC_DIR) 
      #my_dict = {}
+    # if request.method == 'POST':
+    #     form = UploadFileForm(request.POST, request.FILES)
+    #     if form.is_valid():
+    #         handle_uploaded_file(request.FILES['file'])
+    #         #return HttpResponseRedirect('/success/url/')
+    #         # Cycle_Data.objects.all().delete()
+    #         # file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
+    #         # instruction_data = list(csv.reader(open(file_path)))
+    #         # for row in instruction_data:
+    #         #     Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
+    #         # my_list = Cycle_Data.objects.all()
+    #         print("Made it")
+    #         #return render(request, 'home.html', {'instruction_data': my_list})
+    #         Cycle_Data.objects.all().delete()
+    #         return render(request, 'home.html', {'instruction_data': my_list})
+    #     else:
+    #         print("fuck")
+
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        #file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
+        print("lets go!")
+        # return render(request, 'core/simple_upload.html', {
+        #     'uploaded_file_url': uploaded_file_url
+        # })
+
+
     Cycle_Data.objects.all().delete()
     file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
     instruction_data = list(csv.reader(open(file_path)))
     for row in instruction_data:
         Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
-    #     for y in x:
-    #         print(y)
-
-     #d = dict(instruction_data)
     my_list = Cycle_Data.objects.all()
     return render(request, 'home.html', {'instruction_data': my_list})
