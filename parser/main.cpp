39f676fe7304e5f0b2589a3408e56263c1730b0b
ColinAF <cafrisch98@gmail.com>
2022-03-26 16:31:18 -0700

    Fin

--- parser/main.cpp
+++ parser/main.cpp
@@ -3,25 +3,38 @@
 #include "instruction.h"
 #include "debug_macros.h"
 
-
 int main(int argc, char** argv)
 {
-    
-    DEBUG_STDERR("Test Start!\n");
+    if(argc == 2)
+    {
+        DEBUG_STDERR("Test Start!");
 
-    Program p("tests/test0/tst0.asm");
-    DEBUG_STDERR("Parsed the test file!\n");
+        Program p(argv[1]);
+        DEBUG_STDERR("Parsed the test file!");
 
-    p.step_cycle();
-    DEBUG_STDERR("Stepped a cycle!\n");
+        DEBUG_STDERR("Printing the instructions...");
+        p.print_instructions();
 
-    p.back_cycle();
-    DEBUG_STDERR("Went back a cycle!\n");
+        //p.step_cycle();
+        //DEBUG_STDERR("Stepped a cycle!\n");
 
-    p.go_to_line(10);
-    DEBUG_STDERR("Went to line 10!\n");
+        //p.back_cycle();
+        //DEBUG_STDERR("Went back a cycle!\n");
 
-    DEBUG_STDERR("Test Finish!\n");
+        //p.go_to_line(10);
+        //DEBUG_STDERR("Went to line 10!\n");
+
+        p.generate_state_file();
+        DEBUG_STDERR("Generated State file");
+
+        DEBUG_STDERR("Test Finish!\n");
+
+    }
+    else 
+    {
+        DEBUG_STDOUT("USAGE: ./run asm_file\n");
+
+    }
 
     return 0; 
 }
--- parser/program.cpp
+++ parser/program.cpp
@@ -1,28 +1,26 @@
 #include "program.h"
+#include "debug_macros.h"
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <deque>
 
 Program::Program(std::string file_name)
 {
-    input_asm = file_name;
+    input_asm_file = file_name;
     std::ifstream asm_in_stream(file_name, std::ifstream::in);
 
     std::string line;
     std::string instruction_name;
 
-    while (!asm_in_stream.eof())
+    while (asm_in_stream >> line)
     {
-        asm_in_stream >> line;
-        for (size_t i = 0; i < line.size(); ++i)
-        {
-            if (line[i] == ' ')
-            {
-                instruction_name = line.substr(0, i);
-                instructions_vec.push_back(instruction_name);
-            }
-        }
+        instructions_vec.push_back(line);
     }
+
+    generate_states();
+    generate_state_file(); 
 
     return;
 }
@@ -50,7 +48,107 @@ State Program::back_cycle()
     return temp; 
 }
 
+void Program::print_instructions()
+{
+    for (size_t i = 0; i < instructions_vec.size(); ++i)
+    {
+        std::cout << instructions_vec[i] << std::endl;
+    }
+}
+
+// returns pointer to first state
+void Program::generate_states(){
+
+    //start pipeline filled with NOPS
+    std::deque<std::string> pipeline = {"NOP", "NOP", "NOP", "NOP", "NOP"};
+
+    uint64_t cycle_num = 0;
+    cycle_num++;
+    head_state = new State(cycle_num, pipeline[0], pipeline[1], pipeline[2], pipeline[3], pipeline[4]);
+    head_state->prev = head_state;
+    
+    State *prev_state = head_state;
+
+    for (size_t i = 0; i < instructions_vec.size(); ++i)
+    {
+        std::cout << instructions_vec.size() << std::endl;
+
+        std::string instr = instructions_vec[i];
+        pipeline.push_front(instr);
+        pipeline.pop_back();
+        
+        cycle_num++;
+        State *new_state = new State(cycle_num, pipeline[0], pipeline[1], pipeline[2], pipeline[3], pipeline[4]);
+        
+        new_state->prev = prev_state;
+        new_state->prev->next = new_state;
+        new_state->next = NULL;
+
+        prev_state = new_state; 
+
+        // Insert NOPs for Branches!!! 
+        if( instr == "B" || "B.cond")
+        {
+            pipeline.push_front("NOP");
+            pipeline.pop_back();
+            
+            cycle_num++;
+            new_state = new State(cycle_num, pipeline[0], pipeline[1], pipeline[2], pipeline[3], pipeline[4]);
+            
+            new_state->prev = prev_state;
+            new_state->prev->next = new_state;
+            new_state->next = NULL;
+
+            prev_state = new_state;
+
+            pipeline.push_front("NOP");
+            pipeline.pop_back();
+            
+            cycle_num++;
+            new_state = new State(cycle_num, pipeline[0], pipeline[1], pipeline[2], pipeline[3], pipeline[4]);
+            
+            new_state->prev = prev_state;
+            new_state->prev->next = new_state;
+            new_state->next = NULL;
+
+            prev_state = new_state;
+        }
+
+    }
+
+    // Flush out the pipline
+    for (size_t i = 0; i < 5; ++i)
+    {
+        pipeline.push_front("NOP");
+        pipeline.pop_back();
+
+        cycle_num++;
+        State *new_state = new State(cycle_num, pipeline[0], pipeline[1], pipeline[2], pipeline[3], pipeline[4]);
+        new_state->prev = prev_state;
+        new_state->prev->next = new_state;
+        new_state->next = NULL;
+
+        prev_state = new_state; 
+
+    }
+
+    return;
+}
+
 void Program::generate_state_file()
 {
+    std::ofstream csv_out_stream;
+    csv_out_stream.open("statefile.csv", std::ofstream::out);
+
+    State *cur_state = head_state;
+
+    while (cur_state != NULL)
+    {
+        csv_out_stream << cur_state->state_to_csv() << std::endl;
+        cur_state = cur_state->next; 
+    }
+
+    csv_out_stream.close();
+    
     return;
 }
