baee14ef79e322a5075a7cc3bf0406a79aa6fe67
ColinAF <cafrisch98@gmail.com>
2022-03-26 03:13:09 -0700

    added debug macros and a simple test of Program

--- parser/main.cpp
+++ parser/main.cpp
@@ -1,9 +1,38 @@
 
+// Actually might want to but these macros in a header somewhere?
+// Might be usefule elsewhere
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_STDERR(x) (std::cerr << (x))
+#define DEBUG_STDOUT(x) (std::cout << (x))
+#else 
+#define DEBUG_STDERR(x)
+#define DEBUG_STDOUT(x)
+#endif
+
 #include <iostream>
+
+#include "program.h"
 
 int main()
 {
 
-    std::cout << "Parser!\n"; 
+    DEBUG_STDERR("Test Start!\n");
+
+    Program p("tst.asm");
+    DEBUG_STDERR("Parsed the test file!\n");
+
+    p.step_cycle();
+    DEBUG_STDERR("Stepped a cycle!\n");
+
+    p.back_cycle();
+    DEBUG_STDERR("Went back a cycle!\n");
+
+    p.go_to_line(10);
+    DEBUG_STDERR("Went to line 10!\n");
+
+    DEBUG_STDERR("Test Finish!\n");
+
     return 0; 
 }
