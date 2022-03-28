00c7d6609b792fdaf3779a1359e1c9aeffbc1a84
Luke Cordova <lukethegreat000@gmail.com>
2022-03-28 11:46:39 -0700

    style: added styles to highlight the current cycle

--- static/go_to_line.js
+++ static/go_to_line.js
@@ -1,12 +1,21 @@
 
 let codes = document.querySelectorAll('code');
-let cycles = document.querySelectorAll('div.num');
+let cycles = document.querySelectorAll('div.cycle_object');
 let scrollWrapper = document.querySelector('div.cycle_wrapper');
 let current;
+let currentCycle;
 
 for(let i = 0;i<codes.length;i++){
     codes[i].addEventListener('click', () => {
+        
         cycles[i].scrollIntoView(true);
+        if(cycles[i].className.indexOf(' highlight') === -1){
+            if(!!currentCycle){
+                currentCycle.className = currentCycle.className.replace(' highlight', '');
+            }
+            currentCycle = cycles[i];
+            cycles[i].className += ' highlight';
+        }
 
         if(codes[i].className.indexOf(' highlight') === -1){
             if(!!current){
