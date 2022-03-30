6a5e84aa17e650e6e49789f428954bbe64b33ea9
Micah Perez <micahperez99@gmail.com>
2022-03-29 21:00:34 -0700

    refactor: added print statements to home view

--- pipeline/views.py
+++ pipeline/views.py
@@ -9,6 +9,7 @@ from django.core.files.storage import FileSystemStorage
 def home(request):
 
     if request.method == 'POST' and request.FILES['myfile']:
+        print("Received File")
 
         #Use FileSystemStorage() to save the input file to the MEDIA_DIR and save it as user_input.asm
         myfile = request.FILES['myfile']
@@ -18,18 +19,23 @@ def home(request):
         #Check if C executable doesn't exists
         c_executable = os.path.join(PARSER_DIR, '/run')
         if not os.path.exists(c_executable):
+             print("Building C Executable File")
              subprocess.call(["make"], cwd=PARSER_DIR)
+                
 
         #Run users input file using the users input file as a parameter
         subprocess.call(["./run", "user_input.asm"], cwd=PARSER_DIR)
+        print("Running C Executable")
         
         #Make sure there are no objects before creating them
         if (Cycle_Data.objects.exists()):
+            print("Deleting all existing Objects")
             Cycle_Data.objects.all().delete()
 
         #Create objects out for each row in the csv file
         file_path = os.path.join(PARSER_DIR, 'statefile.csv')
         instruction_data = list(csv.reader(open(file_path)))
+        print("Building Instruction Objects")
         for row in instruction_data:
             Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
         instruction_data = Cycle_Data.objects.all()
@@ -40,11 +46,13 @@ def home(request):
 
     #Make sure there are no objects before creating them
     if (Cycle_Data.objects.exists()):
+        print("Deleting all existing Objects")
         Cycle_Data.objects.all().delete()
 
     #Create objects out for each row in the csv file
     file_path = os.path.join(PARSER_DIR, 'tests/tst0/statefile.csv')
     instruction_data = list(csv.reader(open(file_path)))
+    print("Building Instruction Objects")
     for row in instruction_data:
         Cycle_Data(cycle=row[0], fetch=row[1], decode=row[2], execute=row[3], memory=row[4], write_back=row[5]).save()
     my_list = Cycle_Data.objects.all()
