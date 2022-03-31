897e269c1f7a229040838e7a01429bb247946017
Luke Cordova <lukethegreat000@gmail.com>
2022-03-30 18:04:26 -0700

    style: preparing buttons for interacting with the animation

--- static/go_to_line.js
+++ static/go_to_line.js
@@ -3,12 +3,16 @@ let codes = document.querySelectorAll('code');
 let cycles = document.querySelectorAll('div.cycle_object');
 let scrollWrapper = document.querySelector('div.cycle_wrapper');
 let current;
+let currentBtnWrapper;
 let currentCycle;
 
 for(let i = 0;i<codes.length;i++){
     codes[i].addEventListener('click', () => {
         
+
         cycles[i].scrollIntoView(true);
+
+        // highlight cycle
         if(cycles[i].className.indexOf(HIGHLIGHT_STRING) === -1){
             if(!!currentCycle){
                 currentCycle.className = currentCycle.className.replace(HIGHLIGHT_STRING, '');
@@ -17,12 +21,18 @@ for(let i = 0;i<codes.length;i++){
             cycles[i].className += HIGHLIGHT_STRING;
         }
 
+        // highlight selected code, display animation buttons
         if(codes[i].className.indexOf(HIGHLIGHT_STRING) === -1){
             if(!!current){
                 current.className = current.className.replace(HIGHLIGHT_STRING, '');
             }
+            if(!!currentBtnWrapper){
+                currentBtnWrapper.className = currentBtnWrapper.className.replace(HIGHLIGHT_STRING, '');
+            }
             current = codes[i];
             codes[i].className += HIGHLIGHT_STRING;
+            currentBtnWrapper = codes[i].querySelector('div.stepper_button_wrapper');;
+            currentBtnWrapper.className += HIGHLIGHT_STRING;
         }
     });
 }
