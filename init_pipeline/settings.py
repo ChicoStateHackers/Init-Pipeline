cc91a0f646e5788a3a937387d804b7c316f33482
Micah Perez <micahperez99@gmail.com>
2022-06-06 15:37:17 -0700

    chore: added dependencies

--- init_pipeline/settings.py
+++ init_pipeline/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'n/static/'
 STATICFILES_DIRS = [ STATIC_DIR ]
 
 MEDIA_URL = '/parser/'
